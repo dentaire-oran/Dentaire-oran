ca48079d8c411219eb18c29364c20d9988478092
dentaire-oran <benosmanzeriab@gmail.com>
2025-07-15 14:51:00 +0100

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,14 @@
-const CACHE_NAME = 'version-2';
+const CACHE_NAME = 'version-4'; // <-- On passe à la version 4
 // Liste des fichiers à mettre en cache
 const urlsToCache = [
   '/',
   '/index.html',
   '/annee2.html',
-  '/images/icon-192x192.png',
-  '/images/icon-512x512.png'
+  'images/icon-192x192.png', // <-- J'ai enlevé le slash
+  'images/icon-512x512.png'  // <-- J'ai enlevé le slash
 ];
 
+// ... le reste du fichier ne change pas ...
 // Installe le Service Worker et met les fichiers en cache
 self.addEventListener('install', event => {
   event.waitUntil(
@@ -24,7 +25,6 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
-        // Si le fichier est dans le cache, on le retourne. Sinon, on va le chercher sur le réseau.
         return response || fetch(event.request);
       })
   );
